88425dc75503c080d0d259c66a8e39246233b6a6
Sergio <principiante.en.programar@gmail.com>
2024-01-04 16:32:59 -0300

    improve SEO again

--- Componentes/logo.js
+++ Componentes/logo.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image } from "@nextui-org/react";
-export const Logo = () => (
+export const Logo = () => {
 
-  <Image width={"50px"} height={"50px"} style={{marginRight:20+"px"}}src={location.origin+"/logo.png"}/>
+  const [urlOrigin, setUrlOrigin] = useState("");
 
-  // <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-  //   <path
-  //     clipRule="evenodd"
-  //     d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-  //     fill="currentColor"
-  //     fillRule="evenodd"
-  //   />
-  // </svg>
-);
+  useEffect(()=>{
+    setUrlOrigin(location. origin)
+  },[])
+
+  return (
+    <Image width={"50px"} height={"50px"} style={{ marginRight: 20 + "px" }} src={(urlOrigin.includes("localhost"))?"/logo.png":urlOrigin+"/logo.png"} />
+
+    // <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
+    //   <path
+    //     clipRule="evenodd"
+    //     d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+    //     fill="currentColor"
+    //     fillRule="evenodd"
+    //   />
+    // </svg>
+  );
+}
